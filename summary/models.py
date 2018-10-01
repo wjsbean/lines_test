56a0b11b82e89d5a7eb7c02a5c11220d0484cdab
Administrator <wangjinshe@gmail.com>
2018-10-01 09:51:26 +0800

    181001 1st commit

---
+++ summary/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+# Create your models here.
+class Info_TestLines(models.Model):
+    # information for each lines tested
+    pass
+
+
+class Info_EcoCharacter(models.Model):
+    # information for economic characters
+    pass
+
+
+class Info_FieldCharacter(models.Model):
+    # information for field characters
+    pass
+
+
+class Info_ExperiLocation(models.Model):
+    pass
+
+
+class Info_Experiment(models.Model):
+    pass
+
+
+class Criteria_Character(models.Model):
+    pass
+
+
+class PurposeRequirement(models.Model):
+    pass
+
+
+class Info_Yield(models.Model):
+    pass
