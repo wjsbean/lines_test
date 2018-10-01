65e0cfc84eb1319f9451ef0d7677123e6222e6c6
Administrator <wangjinshe@gmail.com>
2018-10-01 10:33:35 +0800

    181001 1st commit

--- summary/models.py
+++ summary/models.py
@@ -3,12 +3,22 @@ from django.db import models
 # Create your models here.
 class Info_TestLines(models.Model):
     # information for each lines tested
-    pass
+    line_name = models.CharField(verbose_name="品系名称", max_length=50)
+    line_institute = models.CharField(verbose_name="选育单位", max_length=50)
+    line_owner = models.CharField(verbose_name="选育人", max_length=10)
+    line_contactor = models.CharField(verbose_name="联系人", max_length=10)
+    line_type = models.CharField(verbose_name="品种类型", max_length=10)
+    line_parents = models.CharField(verbose_name="亲本组合", max_length=50)
+    line_target_date = models.DateField(verbose_name="育成日期")
+    line_phone = models.CharField(verbose_name="联系电话", max_length=15)
+    line_Email = models.EmailField(verbose_name="联系邮箱")
+    exp_name = models.CharField(verbose_name="试验名称", max_length=20)
+    exp_group = models.CharField(verbose_name="试验组别", max_length=20)
 
 
 class Info_EcoCharacter(models.Model):
     # information for economic characters
-    pass
+    line_name = models.CharField(verbose_name="品种名称", max_length=50)
 
 
 class Info_FieldCharacter(models.Model):
